bfbaa917a082d160c253f1adab8ebae47883131f
Tobias S <44494591+Tobio89@users.noreply.github.com>
2026-03-16 19:29:10 +0900

    feat: add rabbitmq handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,17 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const PORT = "80"
 
-type Config struct{}
+type Config struct {
+	Rabbit *amqp.Connection
+}
 
 func main() {
 
+	// connect to RabbitMQ
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+
+	// start listening for messages
+	log.Println("Listening and consuming on RabbitMQ...")
+
 	// this will be config args for the server
-	app := Config{}
+	app := Config{
+		Rabbit: rabbitConn,
+	}
 
 	log.Printf("Starting broker service on port %s\n", PORT)
 
@@ -22,8 +42,38 @@ func main() {
 		Handler: app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+
+	var connection *amqp.Connection
+
+	// wait for RabbitMQ to be ready
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready")
+			counts++
+		} else {
+			connection = c
+			fmt.Println("RabbitMQ connected")
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		fmt.Printf("backing off for %v seconds\n", backOff)
+		time.Sleep(backOff)
+	}
+	return connection, nil
 }
---
+++ event/consumer.go
@@ -0,0 +1,139 @@
+package event
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	err := consumer.setup()
+	if err != nil {
+		return Consumer{}, err
+	}
+
+	return consumer, nil
+}
+
+func (consumer *Consumer) setup() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+	return declareExchange(channel)
+}
+
+func (consumer *Consumer) Listen(topics []string) error {
+	ch, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+	if err != nil {
+		return err
+	}
+
+	for _, topic := range topics {
+		err := ch.QueueBind(
+			q.Name,
+			topic,
+			"logs_topic",
+			false,
+			nil,
+		)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range messages {
+			var payload Payload
+			_ = json.Unmarshal(d.Body, &payload)
+
+			go handlePayload(payload)
+		}
+	}()
+
+	fmt.Printf("Waiting for messages [Exchange: logs_topic, Queue: %s]\n", q.Name)
+
+	<-forever
+
+	return nil
+}
+
+func handlePayload(payload Payload) {
+	switch payload.Name {
+	case "log", "event":
+		err := logEvent(payload)
+		if err != nil {
+			log.Println("Error handling log event:", err)
+		}
+	case "auth":
+	default:
+		log.Printf("Unknown payload name: %s\n", payload.Name)
+		err := logEvent(payload)
+		if err != nil {
+			log.Println("Error handling log event:", err)
+		}
+	}
+
+}
+
+func logEvent(entry Payload) error {
+	// set up json to send to auth service
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+
+	// call auth service
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	// validate response
+	if response.StatusCode != http.StatusAccepted {
+		return err
+	}
+
+	return nil
+
+}
